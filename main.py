1243835f6766f7a57af1d025020993077ad9b403
manavnagpal08 <72203264+manavnagpal08@users.noreply.github.com>
2025-09-22 21:10:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,20 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 st.set_page_config(page_title="ScreenerPro with Watson")
-
+st.markdown(
+    """
+    <script src="//code.tidio.co/c19vp8j19zbvdpbrizjxmw1apt8buoie.js" async></script>
+    <style>
+      /* Move chat to bottom-left */
+      iframe[title="Tidio Chat"] {
+        left: 20px !important;
+        right: auto !important;
+        bottom: 20px !important;
+      }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 st.title(" ScreenerPro with Watson Assistant")
 st.markdown(
     """
