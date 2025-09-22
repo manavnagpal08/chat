dcec91261158d73cbbc701f27a7407df5b60402c
manavnagpal08 <72203264+manavnagpal08@users.noreply.github.com>
2025-09-22 20:04:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import streamlit as st
-import streamlit.components.v1 as components
 
-st.set_page_config(page_title="ScreenerPro with Tidio")
+st.set_page_config(page_title="ScreenerPro with Watson")
 
-st.title("💬 ScreenerPro with Tidio")
+st.title("💬 ScreenerPro with Watson Assistant")
 
-# Inject only the script, no iframe visible
-components.html(
-    """
-    <script src="//code.tidio.co/c19vp8j19zbvdpbrizjxmw1apt8buoie.js" async></script>
-    """,
-    height=0,  # no height
-    width=0    # no width
-)
+# Inject Watson Web Chat
+st.markdown("""
+<script>
+  window.watsonAssistantChatOptions = {
+    integrationID: "49156221-4969-48c2-8924-fd0b623c2f33", // The ID of this integration.
+    region: "au-syd", // The region your integration is hosted in.
+    serviceInstanceID: "bf63ba6f-d81c-4de9-9e39-645fd55f6cbb", // The ID of your service instance.
+    onLoad: async (instance) => { await instance.render(); }
+  };
+  setTimeout(function(){
+    const t=document.createElement('script');
+    t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + 
+           (window.watsonAssistantChatOptions.clientVersion || 'latest') + 
+           "/WatsonAssistantChatEntry.js";
+    document.head.appendChild(t);
+  });
+</script>
+""", unsafe_allow_html=True)
