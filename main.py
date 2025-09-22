df054fffe74265214ce8c10347c72dc904ee3e70
manavnagpal08 <72203264+manavnagpal08@users.noreply.github.com>
2025-09-22 21:06:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ import streamlit.components.v1 as components
 st.set_page_config(page_title="ScreenerPro with Watson")
 
 st.title(" ScreenerPro with Watson Assistant")
-
+st.markdown(
+    """
+    <script src="//code.tidio.co/c19vp8j19zbvdpbrizjxmw1apt8buoie.js" async></script>
+    """,
+    unsafe_allow_html=True
+)
 components.html("""
 <!DOCTYPE html>
 <html>
